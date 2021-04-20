8233273fa6c52a9dbc9e9b393d064836b20efa52
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-04-20 11:52:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
       <input type="text" name="username" id ="username" >
       <br>
       <label for="password">Vaše heslo :</label> 
-      <input type="password" name= 'password' id='password'>
+      <input type="password" name= "password" id="password">
       <br>
-      <input type="submit" value= 'Odeslat' > 
+      <input type="submit" value= "Odeslat" > 
     </form>
 </body>
 </html>
